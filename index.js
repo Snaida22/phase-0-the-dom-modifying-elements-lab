0d3a03e975981882f9af257397937d8c565ef204
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-04 15:22:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+const newHeader= document.createElement('h1')
+ newHeader.id="victory"; //or the code below
+ //newHeader.setAttribute("id", "victory"); 
+ newHeader.innerHTML="Snaida Kabalika is the champion";
